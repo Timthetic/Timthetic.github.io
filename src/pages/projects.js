d5fee4ec038a5784a0f5df32ee9f38bc9ad7d362
Timothy Rediehs <trediehs@g.ucla.edu>
2021-03-23 12:55:28 -0700

    extend bottom margin on project page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,13 +5,21 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ProjectCard from "../components/ProjectCard"
 import { Typography, Grid } from "@material-ui/core"
+import { makeStyles } from '@material-ui/core/styles'
 
 import projectList from "../data/projects"
 
+const useStyles = makeStyles(theme => ({
+	sectionTitle: {
+		marginBottom: '8px'
+	}
+}));
+
 const ProjectsPage = () => {
+	const classes = useStyles();
   return <Layout>
     <SEO title="Projects" />
-    <Typography variant='h5' component='h2'>
+    <Typography variant='h5' component='h2' className={classes.sectionTitle}>
 			Projects
 		</Typography>
 		<Grid container spacing={1}>
